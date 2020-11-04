4fd7aebe84066d545b57274086806eb84ab0a1db
Andrew.K <akirillychev@yahoo.com>
2020-11-04 10:10:02 -0500

    updated

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+import Header from './components/Header';
+import About from './components/About';
+import Work from './components/work';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <div className="App-header">
+      <Header></Header>
+      </div>
+      
+      
+      <div className='work'>
+        <Work></Work>
+      </div>
+      <div className='contact'>
+        <Contact></Contact>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,45 @@
+export default
+    {
+      headerTagline: [
+    'Full stack developer who likes to keep it simple and easy!',
+    ],
+    
+        contactEmail:'kirillychev.Andrew@gmail.com',
+       
+         projects:[
+           {
+            id: 1,
+            title:'Password Generator', 
+             imageSrc:"https://lh3.googleusercontent.com/zsfUNxG2Ld4F341xViwH3d_I-SKJ3cKPmywDDTZIT6Vq0WGj0RladvBZCXLM3EKXZ0F0=s156",
+             url:'https://github.com/akirillychev/Password-Generator-Project'
+            },
+            {
+                id: 2,
+                title:'Send Foodz', 
+                imageSrc:"https://raw.githubusercontent.com/akirillychev/send-foodz/master/assets/images/home-screenshot.png",
+                url:'https://github.com/akirillychev/send-foodz'
+               },
+            { 
+                id: 3,
+                title:'Weather Dashboard', 
+                imageSrc:"https://raw.githubusercontent.com/akirillychev/Weather-Dashboard/master/assets/Capture.PNG",
+                url:'https://github.com/akirillychev/Weather-Dashboard'
+               },
+            {
+                id: 4,
+                title: 'Refactor Project',
+                imageSrc: "https://raw.githubusercontent.com/akirillychev/My-Refactor-Project/master/assets/refactor%20screenshot%202.PNG",
+                url: 'https://github.com/akirillychev/My-Refactor-Project'
+           }
+        ],
+        social: [
+            
+            {   name:'Github',
+                url:'https://github.com/akirillychev'},
+            {
+                name: 'linkedin',
+                url: 'https://ch.linkedin.com/in/andrew-kirillychev-7a4aa082/de'
+            }
+
+        ]
+    }
